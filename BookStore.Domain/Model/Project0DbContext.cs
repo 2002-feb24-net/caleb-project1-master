9a05c7c5e593dd62d7efe031e0e782eef73138ac
Caleb Crownover <cj.crown@hotmail.com>
2020-04-06 20:51:02 -0500

    completed db reconstruct

--- BookStore.Domain/Model/Project0DbContext.cs
+++ BookStore.Domain/Model/Project0DbContext.cs
@@ -22,14 +22,7 @@ namespace BookStore.Domain.Model
         public virtual DbSet<Products> Products { get; set; }
         public virtual DbSet<Stores> Stores { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:2002-training-crownover.database.windows.net,1433;Initial Catalog=Project0Db;Persist Security Info=False;User ID=caleb;Password=Revature-2002;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
